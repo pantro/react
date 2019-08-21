50218a9b1acbcd69309f668c64a45c0f7b280c2f
Luna <francoguns16@gmail.com>
2019-08-21 15:54:01 -0500

    WeatherAPP: Creando tres componentes

---
+++ WeatherApp/weather-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import WeatherLocation from './components/WeatherLocation';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Weather App (Aplicación del clima)
+      <WeatherLocation/>
+    </div>
+  );
+}
+
+export default App;
