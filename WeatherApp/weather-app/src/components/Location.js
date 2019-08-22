9191c98c2e47a0babc830479cac8dc831d36ca3a
Luna <francoguns16@gmail.com>
2019-08-22 16:40:23 -0500

    WeatherApp: Utilizando parametros y tipandolos

--- WeatherApp/weather-app/src/components/Location.js
+++ WeatherApp/weather-app/src/components/Location.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Location = () => (
-	<div><h1>Buenos Aires</h1></div>	
-);
+const Location = (props) => {
+	//Destructuring
+	//const city= props.city;
+	const { city } = props;
+	
+	return (
+		<div><h1>{city}</h1></div>	
+	);
+};
+
+Location.propTypes = {
+	city: PropTypes.string.isRequired,
+};
 
 export default Location;
--- WeatherApp/weather-app/src/components/WeatherData.js
+++ WeatherApp/weather-app/src/components/WeatherData.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+import {
+	SUN,
+} from './../constants/weathers';
 
 const WeatherData = () => (
-	<div>Weather Data</div>
+	<div>
+		<WeatherTemperature 
+			temperature = {20} 
+			weatherState = {SUN}
+		/>
+		<WeatherExtraInfo humidity={80} wind = {"10 m/s"}/>
+	</div>
 );
 
 export default WeatherData;
