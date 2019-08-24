95c35a095a866cb68819156c830fdcdecc5de5e7
Luna <francoguns16@gmail.com>
2019-08-23 19:17:51 -0500

    WeatherApp: Contruyendo clases desde funciones

--- WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/WeatherData/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-	SUN,
-} from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (
+	
 	<div className="weatherDataCont">
 		<WeatherTemperature 
-			temperature = {20} 
-			weatherState = {SUN}
+			temperature = {temperature} 
+			weatherState = {weatherState}
 		/>
-		<WeatherExtraInfo humidity={80} wind = {"10 m/s"}/>
+		<WeatherExtraInfo humidity={humidity} wind = {wind}/>
 	</div>
 );
+
+WeatherData.propTypes = {
+	data: PropTypes.shape({
+		temperature: PropTypes.number.isRequired,
+		weatherState: PropTypes.string.isRequired,
+		humidity: PropTypes.number.isRequired,
+		wind: PropTypes.string.isRequired,
+	}),
+}
 
 export default WeatherData;
--- WeatherApp/weather-app/src/components/WeatherLocation/index.js
+++ WeatherApp/weather-app/src/components/WeatherLocation/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import {
+	CLOUD,
+	CLOUDY,
+	SUN,
+	RAIN,
+	SNOW,
+	WINDY, 
+} from './../../constants/weathers';
 
-const WeatherLocation = () => (
-	<div className="weatherLocationCont">
-	  <Location city = {"Buenos Aires"}/>
-	  <WeatherData/>
-    </div>
-);
+const data = {
+	temperature: 5,
+	weatherState: SUN,
+	humidity:10,
+	wind: '10 m/s',
+}
+const data2 = {
+	temperature: 8,
+	weatherState: WINDY,
+	humidity:20,
+	wind: '20 m/s',
+}
+
+class WeatherLocation extends Component {
+	constructor() {
+		super();
+		this.state = {
+			city: 'Buenos Aires',
+			data: data,
+		};
+	}
+
+	handleUpdateClick = () => {
+		this.setState({
+			city: 'Buenos Aires!',
+			data: data2,
+		});
+	}
+	
+	render() {
+		const { city, data } = this.state;
+		return (
+			<div className="weatherLocationCont">
+				<Location city = {city} />
+				<WeatherData data={data} />
+				<button onClick= {this.handleUpdateClick}>Actualizar</button>
+			</div>
+		);
+	}
+}
 
 export default WeatherLocation;
